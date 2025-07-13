3c7ec861b65faca6ed2d25c0817cae5fe5a613d5
Sujitha K C <sujithakanagarathinam@gmail.com>
2025-07-13 16:20:09 +0530

    Added Exercise 4

---
+++ Exercise 4_counter/app.js
@@ -0,0 +1,20 @@
+// app.js
+const app = angular.module('counterApp', []);
+
+app.controller('counterCtrl', function($scope) {
+  $scope.count = 0;
+
+  $scope.increment = function() {
+    $scope.count++;
+  };
+
+  $scope.decrement = function() {
+    if ($scope.count > 0) {
+      $scope.count--;
+    }
+  };
+
+  $scope.reset = function() {
+    $scope.count = 0;
+  };
+});
